950d9fe79899c6b0b283c5a71b76779a02b1c543
kdg1986 <rycu@naver.com>
2021-08-06 23:34:42 +0900

    mod config

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = (env, options) => {
           use: [
             {
               loader: "html-loader",
-              options: { minimize: false /* 최적화옵션*/ } 
+              options: { minimize: true } 
             }
           ]
         },
